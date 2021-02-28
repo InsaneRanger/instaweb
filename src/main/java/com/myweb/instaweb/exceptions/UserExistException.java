5d34629b1a417db81fb37711311d5e8a66d76b9f
Insane <valeev.timur.v@gmail.com>
2021-02-28 20:45:08 +0300

    Add custom annotation and Payload service, validation

---
+++ src/main/java/com/myweb/instaweb/exceptions/UserExistException.java
@@ -0,0 +1,21 @@
+package com.myweb.instaweb.exceptions;
+
+
+
+
+/*
+ * @author
+ * @version
+ * @return
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserExistException extends RuntimeException {
+
+    public UserExistException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/myweb/instaweb/service/UserService.java
@@ -0,0 +1,51 @@
+package com.myweb.instaweb.service;
+
+
+
+
+/*
+ * @author
+ * @version
+ * @return
+ */
+
+import com.myweb.instaweb.entity.User;
+import com.myweb.instaweb.entity.roles.Roles;
+import com.myweb.instaweb.exceptions.UserExistException;
+import com.myweb.instaweb.payload.request.SignupRequest;
+import com.myweb.instaweb.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+
+    /** Mapping model attribute and Bcrypt password from database  */
+    public User createUser(SignupRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(Roles.ROLE_USER);
+
+        try {
+            log.info("Saving user {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception exception){
+            log.error("Error during registration {}", exception.getMessage());
+            throw new UserExistException("The user " + user.getUsername() + " alredy exist. Please check conditionals");
+        }
+    }
+}
