7ed9cc65af3c5477630cce22ea8ae2bcb617a635
Insane <valeev.timur.v@gmail.com>
2021-02-28 17:05:47 +0300

    creating  layer repository

---
+++ src/main/java/com/myweb/instaweb/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package com.myweb.instaweb.repository;
+
+import com.myweb.instaweb.entity.Post;
+import com.myweb.instaweb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+
+    List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+
+    List<Post> findAllByOrderByCreatedDateDesc();
+
+    Optional<Post> findPostByIdAndUser(Long id);
+
+}
